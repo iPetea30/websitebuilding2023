ff2a7a2e08589061d0fd3e338597455f191063f0
Frontend JavaScript <frontendjavascript@C3-14.local>
2023-03-07 20:47:11 +0200

    curs3

--- curs3/solutii/restfull/user.js
+++ curs3/solutii/restfull/user.js
@@ -3,27 +3,27 @@
 
 
 // Get all users
-function httpGet(){
-var url  = "http://localhost/javascript/curs5/restfull/users.php";
-var xhr  = new XMLHttpRequest();
-xhr.open('GET', url, true)
-//xhr.readystatechange = function () {
-//https://teamtreehouse.com/community/xhronreadystatechange-vs-xhronload
-xhr.onload = function () {
-	var users = JSON.parse(xhr.responseText);
-	if (xhr.readyState == 4 && xhr.status == "200") {
-		console.table(users);
-	} else {
-		console.error(users);
+function httpGet() {
+	var url = "http://localhost/restfull/users.php";
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true)
+	//xhr.readystatechange = function () {
+	//https://teamtreehouse.com/community/xhronreadystatechange-vs-xhronload
+	xhr.onload = function () {
+		var users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "200") {
+			console.table(users);
+		} else {
+			console.error(users);
+		}
 	}
-}
-xhr.send(null);
+	xhr.send(null);
 }
 
 //Get a user
-var url  = "http://localhost/javascript/curs5/restfull/users.php";
-var xhr  = new XMLHttpRequest()
-xhr.open('GET', url+'?id=2', true)
+var url = "http://localhost/restfull/users.php";
+var xhr = new XMLHttpRequest()
+xhr.open('GET', url + '?id=2', true)
 xhr.onload = function () {
 	var users = JSON.parse(xhr.responseText);
 	if (xhr.readyState == 4 && xhr.status == "200") {
@@ -36,42 +36,46 @@ xhr.send(null);
 
 
 // Post a user
-// var url = "http://localhost/javascript/curs5/restfull/users.php";
+function addUser() {
 
-// var data = 'id=4&nume=Vasiliescu&prenume=Vlad';
+	var url = "http://localhost/javascript/curs5/restfull/users.php";
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", url, true);
-// xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-// xhr.onload = function () {
-// 	var users = JSON.parse(xhr.responseText);
-// 	if (xhr.readyState == 4 && xhr.status == "200") {
-// 		console.table(users);
-// 	} else {
-// 		console.error(users);
-// 	}
-// }
-// xhr.send(data);
+	var data = 'id=4&nume=Vasiliescu&prenume=Vlad';
 
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.onload = function () {
+		var users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "200") {
+			console.table(users);
+		} else {
+			console.error(users);
+		}
+	}
+	xhr.send(data);
 
+}
 // Update a user
-// var url = "http://localhost/javascript/curs5/restfull/users.php";
+function editUser() {
 
-// var data = 'nume=Anton&prenume=Vlad';
+	var url = "http://localhost/javascript/curs5/restfull/users.php";
 
-// var xhr = new XMLHttpRequest();
-// xhr.open("PUT", url+'?id=1', true);
-// xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
-// xhr.onload = function () {
-// 	var users = JSON.parse(xhr.responseText);
-// 	if (xhr.readyState == 4 && xhr.status == "200") {
-// 		console.table(users);
-// 	} else {
-// 		console.error(users);
-// 	}
-// }
-// xhr.send(data);
+	var data = 'nume=Anton&prenume=Vlad';
 
+	var xhr = new XMLHttpRequest();
+	xhr.open("PUT", url + '?id=1', true);
+	xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	xhr.onload = function () {
+		var users = JSON.parse(xhr.responseText);
+		if (xhr.readyState == 4 && xhr.status == "200") {
+			console.table(users);
+		} else {
+			console.error(users);
+		}
+	}
+	xhr.send(data);
+}
 
 // Delete a user
 // var url = "http://localhost/javascript/curs5/restfull/users.php";
